351cf7887d5f7495e1cf627444015e69a3add9fc
yoseio <98276492+yoseio@users.noreply.github.com>
2024-09-23 15:12:23 +0900

    feat: add switch command for nixosConfigurations

--- src/blockTypes/nixosConfigurations.nix
+++ src/blockTypes/nixosConfigurations.nix
@@ -18,6 +18,31 @@
     # nixosGenerator's actions?
     # microvm action?
     # nixos-rebuild action?
+    actions = {
+      currentSystem,
+      fragment,
+      fragmentRelPath,
+      target,
+      inputs,
+    }: let
+      getString = o: (l.elemAt (l.splitString ["/"] fragmentRelPath) o);
+      host = (getString 0) + "-" + (getString 2);
+      dc = getString 1;
+      bin = ''
+        bin=$(nix build .#${dc}.${host}.system --no-link --print-out-paths)/sw/bin
+        export PATH=$bin:$PATH
+      '';
+    in [
+      (mkCommand currentSystem {
+        name = "switch";
+        description = "switch the configuration";
+        command =
+          bin
+          + ''
+            nixos-rebuild switch --flake .#${host}
+          '';
+      })
+    ];
   };
 in
   nixosConfigurations
